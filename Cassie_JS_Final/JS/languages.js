46f0dd48fc2a6fc942ef80e2bc1260cdb4b65233
Cocoa243 <64682308+Cocoa243@users.noreply.github.com>
2020-07-09 21:15:34 +0200

    Add files via upload

---
+++ Cassie_JS_Final/JS/languages.js
@@ -0,0 +1,206 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Handler when the DOM is fully loaded
+  });
+//Event listener for each answers
+//const Q1A1 = document.getElementById ("techo");
+//techo.addEventListener ('click', function (){
+  //  alert("Sorry, this is not correct. Techo means “roof”.")
+//});
+//const Q1A2 = document.getElementById ("mesa");
+//mesa.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right. Mesa is table in Spanish")
+//})
+//const Q1A3 = document.getElementById ("puerta");
+//puerta.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. Puerta means “door”")
+//})
+//const Q1A4 = document.getElementById ("tabla");
+//puerta.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. Tabla means “board, floorboard”")
+//})
+
+
+//const Q2A1 = document.getElementById ("seven");
+//seven.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right.This sentence contains all of them: ‘He believed Caesar could see people seizing the seas’.")
+//})
+//const Q2A2 = document.getElementById ("five");
+//five.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct but did you know that there are five vowels in English?(a,e,i,o,u)")
+//})
+//const Q2A3 = document.getElementById ("six");
+//six.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct but did you know that 'almost' is the only English word that has 6 letters arranged alphabetically?")
+//})
+//const Q2A4 = document.getElementById ("nine");
+//nine.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct but did you know that 'noon' originally meant the ninth hour of the day after sunrise, which was around 3pm?")
+//})
+
+//const Q3A1 = document.getElementById ("Sudan");
+//Sudan.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. The offcial languages of Sudan are Arabic and English.")
+//})
+//const Q3A2 = document.getElementById ("Somalia");
+//Somalia.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The offcial languages of Somalia are Somali and Arabic.")
+//})
+//const Q3A3 = document.getElementById ("SouthSudan");
+//SouthSudan.addEventListener ('click', function (){
+//   alert("Sorry, this is not correct. Tigrinya is not among South Sudan official languages")
+//})
+//const Q3A4 = document.getElementById ("Eritrea");
+//Eritrea.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right.Tigrinya language is an official language in Eritrea, Ethiopia and  in the Tigray region.")
+//})
+
+
+//const Q4A1 = document.getElementById ("big");
+//big.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The word 'big' is 'nla' in Yoruba.")
+//})
+//const Q4A2 = document.getElementById ("sunny");
+//sunny.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The word 'sunny' is 'oorun' in Yoruba.")
+//})
+//const Q4A3 = document.getElementById ("beautiful");
+//beautiful.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right. 'Orile ede to rewa ni nigeria' means 'Nigeria is a beautiful country'.")
+//})
+//const Q4A4 = document.getElementById ("blessed");
+//blessed.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The word 'blessed' is 'ibukun' in Yoruba.")
+//})
+
+
+//const Q5A1 = document.getElementById ("swahili");
+//swahili.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right. Pongezi")
+//})
+//const Q5A2 = document.getElementById ("shona");
+//shona.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. Hapana")
+//})
+//const Q5A3 = document.getElementById ("zulu");
+//zulu.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. Hapana")
+//})
+//const Q5A4 = document.getElementById ("oromo");
+//oromo.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. Hapana")
+//})
+
+//const Q6A1 = document.getElementById ("exactsame");
+//exactsame.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. Hmmm not this one,  ")
+//})
+//const Q6A2 = document.getElementById ("notrelated");
+//notrelated.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. Do you speak French? ")
+//})
+//const Q6A3 = document.getElementById ("slightdifference");
+//slightdifference.addEventListener ('click', function (){
+//    alert("Congratulations, you are right.In English 'raisin' is a a dried grape while in French 'raisin' is the equivalent of 'grape'")
+//})
+//const Q6A4 = document.getElementById ("nounadjective");
+//nounadjective.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. Did you know that In English we pronounce ˈreɪzn̩ (rei-zn) , in French ʁezɛ̃ (ré-zin)")
+     
+//})
+
+
+
+//const Q7A1 = document.getElementById ("lingala");
+//lingala.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The translation in lingala means 'I love you'.")
+//})
+//const Q7A2 = document.getElementById ("italian");
+//italian.addEventListener ('click', function (){
+  //  alert("Sorry, this is not correct. The translation in italian means 'I love you'.")
+//})
+//const Q7A3 = document.getElementById ("korean");
+//korean.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. The translation in korean means 'I love you'.")
+//})
+//const Q7A4 = document.getElementById ("dutch");
+//dutch.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right. 'Ik heb honger' does NOT mean 'I love you' but 'I am hungry'.")
+//})
+
+
+//const Q8A1 = document.getElementById ("nigercongo");
+//nigercongo.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. An exemple from this language family is Wolof.")
+//})
+//const Q8A2 = document.getElementById ("afroasiatic");
+//afroasiatic.addEventListener ('click', function (){
+//    alert("Congratulations, you are right. Arabic belongs to the Afro-Asiatic language family.Hebrew, Hausa and Kabyle are other widely spoken Afro-Asiatic languages.  ")
+//})
+//const Q8A3 = document.getElementById ("pamanyungan");
+//pamanyungan.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. An exemple from this language family is Wajarri.")
+//})
+//const Q8A4 = document.getElementById ("indoeuropean");
+//indoeuropean.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct.An exemple from this language family is Armenian. ")
+//})
+
+
+//const Q9A1 = document.getElementById ("saudade");
+//saudade.addEventListener ('click', function (){
+ //   alert("Congratulations, you are right. The latin word for saudade is 'desiderium'.")
+//})
+//const Q9A2 = document.getElementById ("saude");
+//saude.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. 'Saude' means 'health'")
+//})
+//const Q9A3 = document.getElementById ("sagacidade");
+//sagacidade.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. 'Sagacidade' means 'wit'. ")
+//})
+//const Q9A4 = document.getElementById ("sumidade");
+//sumidade.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. 'Sumidade' means 'high priest/priestess' ")
+//})
+
+//const Q10A1 = document.getElementById ("alkaa");
+//alkaa.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. This word means 'start'.")
+//})
+//const Q10A2 = document.getElementById ("kavella");
+//kavella.addEventListener ('click', function (){
+ //   alert("Sorry, this is not correct. This word means 'walk'. ")
+//})
+//const Q10A3 = document.getElementById ("choklad");
+//choklad.addEventListener ('click', function (){
+//    alert("Sorry, this is not correct. This word means 'chocolate'. ")
+//})
+//const Q10A4 = document.getElementById ("paa");
+//paa.addEventListener ('click', function (){
+//    alert("Congratulations, you are right. This is the end.")
+//})
+
+//Total results for the quiz - using loop
+//var i = document.getElementById("quizlanguages");
+//function countScore (
+//    var i = document.getElementById("quizlanguages");   
+//)
+const myForm= document.getElementsByTagName ("input");
+myForm.onsubmit= function(){
+let correctAnswers = document.getElementsByClassName ("correct");
+let wrongAnswers= document.getElementsByClassName ("wrong");
+
+var yourResults = (myForm - wrongAnswers) + correctAnswers;
+};
+//value = "correct"
+//let idioms = document.querySelectorAll(".correct");
+//idioms.forEach(element => {console.log(element)
+//}
+//);
+
+//get the value attribute to the inside the for each function. Get the value from the element , which is the input
+//let yourResults = 
+//const correctAnswers = document.querySelectorAll ("correct");
+//correctAnswers.forEach
+
+//yourResults++
